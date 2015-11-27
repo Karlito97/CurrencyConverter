7db7ed8134958a24bba9b923300e7ac41c56357b
Karlito97 <salecharlesjr@yahoo.fd>
2015-11-27 03:08:01 +0100

    Création du formulaire Converter et création de la classe Currency

---
+++ src/main/java/com.converter/model/Currency.java
@@ -0,0 +1,71 @@
+package com.converter.model;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+
+/**
+ * Created by Charles Jr on 26/11/2015.
+ */
+public class Currency {
+
+    //Attributs
+    private StringProperty identifiant;
+    private StringProperty devise;
+    private SimpleDoubleProperty taux;
+
+    /**
+     * Propriétés : Accesseurs et Mutateurs
+     */
+    public String getIdentifiant() {
+        return identifiant.get();
+    }
+
+    public void setIdentifiant(String identifiant) {
+        this.identifiant.set(identifiant);
+    }
+
+    public StringProperty identifiantProperty() {
+        return identifiant;
+    }
+
+    public String getDevise() {
+        return devise.get();
+    }
+
+    public void setDevise(String devise) {
+        this.devise.set(devise);
+    }
+
+    public StringProperty deviseProperty() {
+        return devise;
+    }
+    public SimpleDoubleProperty getTaux() {
+        return taux;
+    }
+
+    public void setTaux(double taux) {
+        this.taux.set(taux);
+    }
+
+    /**
+     * Default constructor.
+     */
+    public Currency() {
+        this(null, null, 0);
+    }
+
+    /**
+     * Constructor .
+     *
+     * @param identifiant
+     * @param devise
+     * @param taux
+     */
+    public Currency(String identifiant, String devise, double taux) {
+        this.identifiant = new SimpleStringProperty(identifiant);
+        this.devise = new SimpleStringProperty(devise);
+        this.taux = new SimpleDoubleProperty(taux);
+    }
+}
---
+++ src/main/java/com.converter/view/AccueilController.java
@@ -0,0 +1,10 @@
+package com.converter.view;
+
+import sun.applet.Main;
+
+/**
+ * Created by Charles Jr on 26/11/2015.
+ */
+public class AccueilController {
+    private Main main;
+}
